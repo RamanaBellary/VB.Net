ce22d9b7e602b539f8cd1837555cf5a319847962
Ramana <ramanayadavs@gmail.com>
2018-09-24 22:32:07 +0530

    Initial commit

---
+++ ADO.Net_Samples/ADO.Net_Samples/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.dgv1 = New System.Windows.Forms.DataGridView()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnCommit = New System.Windows.Forms.Button()
+        Me.btnRollback = New System.Windows.Forms.Button()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnBulkCopy = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.dgv1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.dgv1)
+        Me.Panel1.Location = New System.Drawing.Point(-3, 86)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(803, 345)
+        Me.Panel1.TabIndex = 0
+        '
+        'dgv1
+        '
+        Me.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgv1.Location = New System.Drawing.Point(0, 0)
+        Me.dgv1.Name = "dgv1"
+        Me.dgv1.Size = New System.Drawing.Size(803, 345)
+        Me.dgv1.TabIndex = 0
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(325, 37)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 1
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnCommit
+        '
+        Me.btnCommit.Location = New System.Drawing.Point(424, 37)
+        Me.btnCommit.Name = "btnCommit"
+        Me.btnCommit.Size = New System.Drawing.Size(75, 23)
+        Me.btnCommit.TabIndex = 2
+        Me.btnCommit.Text = "Commit"
+        Me.btnCommit.UseVisualStyleBackColor = True
+        '
+        'btnRollback
+        '
+        Me.btnRollback.Location = New System.Drawing.Point(524, 37)
+        Me.btnRollback.Name = "btnRollback"
+        Me.btnRollback.Size = New System.Drawing.Size(75, 23)
+        Me.btnRollback.TabIndex = 3
+        Me.btnRollback.Text = "Rollback"
+        Me.btnRollback.UseVisualStyleBackColor = True
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(225, 37)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(75, 23)
+        Me.btnLoad.TabIndex = 4
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnBulkCopy
+        '
+        Me.btnBulkCopy.Location = New System.Drawing.Point(627, 37)
+        Me.btnBulkCopy.Name = "btnBulkCopy"
+        Me.btnBulkCopy.Size = New System.Drawing.Size(75, 23)
+        Me.btnBulkCopy.TabIndex = 5
+        Me.btnBulkCopy.Text = "Bulk Copy"
+        Me.btnBulkCopy.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.lblStatus.Location = New System.Drawing.Point(0, 437)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(800, 13)
+        Me.lblStatus.TabIndex = 6
+        Me.lblStatus.Text = "..."
+        Me.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnBulkCopy)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.btnRollback)
+        Me.Controls.Add(Me.btnCommit)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.dgv1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents dgv1 As DataGridView
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnCommit As Button
+    Friend WithEvents btnRollback As Button
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnBulkCopy As Button
+    Friend WithEvents lblStatus As Label
+End Class
---
+++ ADO.Net_Samples/ADO.Net_Samples/Form1.vb
@@ -0,0 +1,166 @@
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
+Imports System.Threading
+
+Public Class Form1
+    Private connectionString As String
+    Private sda As SqlDataAdapter
+    Private ds As DataSet
+    Private con As SqlConnection
+    Private tran As SqlTransaction
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connectionString = ConfigurationManager.ConnectionStrings("SampleDB").ConnectionString
+        con = New SqlConnection(connectionString)
+        btnLoad.Enabled = True
+        btnUpdate.Enabled = False
+        btnRollback.Enabled = False
+        btnCommit.Enabled = False
+        'UsingDataAdapter()
+        'UsingStoredProcedure()
+        'UsingSqlCommandBuilder()
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, args As EventArgs) Handles MyBase.FormClosing
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+    End Sub
+
+    Private Sub UsingDataAdapter()
+        Try
+            Using sda = New SqlDataAdapter("select * from tCustomers", con)
+                Dim ds As New DataSet
+                sda.Fill(ds)
+                dgv1.DataSource = ds.Tables(0)
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub UsingStoredProcedure()
+        Try
+            Dim cmd As New SqlCommand("GetCustomer", con)
+            cmd.Parameters.AddWithValue("@CustomerID", 1)
+            cmd.CommandType = CommandType.StoredProcedure
+            con.Open()
+            'Dim source As New BindingSource
+            'source.DataSource = cmd.ExecuteReader()
+            'DataGridView1.DataSource = source
+            Dim dt As New DataTable()
+            dt.Load(cmd.ExecuteReader)
+            dgv1.DataSource = dt
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub UsingSqlCommandBuilder()
+        If con.State = ConnectionState.Closed Then
+            con.Open()
+            tran = con.BeginTransaction()
+        End If
+        Dim sqlCmd = New SqlCommand("select * from tCustomers", con, tran)
+        sda = New SqlDataAdapter(sqlCmd)
+        ds = New DataSet
+        sda.Fill(ds)
+        dgv1.DataSource = ds.Tables(0)
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim scb As New SqlCommandBuilder(sda)
+        sda.Update(ds)
+        btnRollback.Enabled = True
+        btnCommit.Enabled = True
+        btnUpdate.Enabled = False
+        btnLoad.Enabled = False
+    End Sub
+
+    Private Sub btnCommit_Click(sender As Object, e As EventArgs) Handles btnCommit.Click
+        Try
+            tran.Commit()
+            con.Close()
+            btnLoad.Enabled = True
+            btnUpdate.Enabled = False
+            btnCommit.Enabled = False
+            btnRollback.Enabled = False
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnRollback_Click(sender As Object, e As EventArgs) Handles btnRollback.Click
+        Try
+            tran.Rollback()
+            con.Close()
+            btnLoad.Enabled = True
+            btnUpdate.Enabled = False
+            btnRollback.Enabled = False
+            btnCommit.Enabled = False
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        UsingSqlCommandBuilder()
+        btnLoad.Enabled = False
+        btnRollback.Enabled = False
+        btnCommit.Enabled = False
+        btnUpdate.Enabled = True
+    End Sub
+
+    Private Sub btnBulkCopy_Click(sender As Object, e As EventArgs) Handles btnBulkCopy.Click
+        bulkCopy()
+        lblStatus.Text = "Bulk Copying.."
+    End Sub
+
+    Private Async Sub bulkCopy()
+        Await Task.Run(Sub()
+                           Try
+                               Thread.Sleep(5000)
+                               Dim dt As New DataTable
+                               dt.Columns.Add("CustomerName")
+                               dt.Columns.Add("Age")
+                               dt.Columns.Add("Address")
+                               dt.Columns.Add("Gender")
+                               Dim dr As DataRow = dt.NewRow()
+                               dr("CustomerName") = "Ramana"
+                               dr("Age") = 34
+                               dr("Address") = "Bangalore"
+                               dr("Gender") = "M"
+                               dt.Rows.Add(dr)
+
+                               dr = dt.NewRow()
+                               dr("CustomerName") = "Mounika"
+                               dr("Age") = 24
+                               dr("Address") = "Bangalore"
+                               dr("Gender") = "F"
+                               dt.Rows.Add(dr)
+
+                               Using bulkCopy As New SqlBulkCopy(con)
+                                   bulkCopy.DestinationTableName = "tCustomers"
+                                   bulkCopy.ColumnMappings.Add("CustomerName", "CustomerName")
+                                   bulkCopy.ColumnMappings.Add("Age", "Age")
+                                   bulkCopy.ColumnMappings.Add("Address", "Address")
+                                   bulkCopy.ColumnMappings.Add("Gender", "Gender")
+
+                                   If con.State = ConnectionState.Closed Then
+                                       con.Open()
+                                   End If
+                                   bulkCopy.WriteToServerAsync(dt)
+                                   lblStatus.Invoke(Sub()
+                                                        lblStatus.Text = "Done with bulk copy.."
+                                                        Refresh()
+                                                    End Sub)
+
+                               End Using
+                           Catch ex As Exception
+                               MessageBox.Show(ex.Message)
+                           End Try
+                       End Sub)
+
+    End Sub
+End Class
